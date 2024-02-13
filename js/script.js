50f7c038e6c624fe2a556294e30ccf9fe42cbef9
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-13 14:21:27 +0100

    built repo, added array

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+
+// Wayne Barnett	 Founder & CEO	         wayne-barnett-founder-ceo.jpg
+// Angela Caroll	 Chief Editor	         angela-caroll-chief-editor.jpg
+// Walter Gordon	 Office Manager	         walter-gordon-office-manager.jpg
+// Angela Lopez	 Social Media Manager 	 angela-lopez-social-media-manager.jpg
+// Scott Estrada	 Developer	             scott-estrada-developer.jpg
+// Barbara Ramos	 Graphic Designer	     barbara-ramos-graphic-designer.jpg
+
+
+
+
+let team= [
+    {
+        name: "Wayne",
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Walter",
+        lastName: "Gordon",
+        role: "Office Manager",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        img: "wayne-barnett-founder-ceo.jpg"
+
+    },
+    {
+        name: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        img: "wayne-barnett-founder-ceo.jpg"
+
+    },
+    {
+        name: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        img: "wayne-barnett-founder-ceo.jpg"
+
+    }
+];
+
+
+
+
+
+
+
+
